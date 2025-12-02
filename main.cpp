e4768ba48f22cfc51c427f3a1f95f42ed1ac3072
Alexander <aldram@Zenbook.localdomain>
2025-12-02 09:37:46 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace topit {
+    struct p_t {
+	int x, y; 
+    };
+    bool operator==(p_t a, p_t b);
+    bool operator!=(p_t a, p_t b);
+}
+
+int main() {
+    using namespace topit;
+    p_t a{1, 1}, b{0, 1};
+    std::cout << (a == b) << std::endl;
+}
+
+bool topit::operator==(p_t a, p_t b) {
+        return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b) {
+    return !(a == b);
+}
